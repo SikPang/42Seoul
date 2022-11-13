bc7e7e6baa0ce469f10d8aab5b280f5b12ab2208
SikPang <87380790+SikPang@users.noreply.github.com>
2022-11-13 23:29:54 +0900

    part 1

--- 1. libft/ft_atoi.c
+++ 1. libft/ft_atoi.c
@@ -6,34 +6,42 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 21:06:44 by kwsong            #+#    #+#             */
-/*   Updated: 2022/11/13 17:49:50 by kwsong           ###   ########.fr       */
+/*   Updated: 2022/11/13 23:29:17 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
+#include <stdio.h>
 
-static int	check_over_flow(int before_num, char ch, int minus)
+static int	check_over_flow(long before_num, char ch, int minus)
 {
-	int	after_num;
+	long	after_num;
 
-	after_num = before_num * 10 + ch - '0';
-	if (after_num < 0 && before_num > 0)
+	if (minus == 1)
 	{
-		if (minus == -1)
+		after_num = before_num * 10 + ch - '0';
+		if (after_num < 0 && before_num > 0)
+			return (1);
+	}
+	else
+	{
+		before_num *= -1;
+		after_num = before_num * 10 - (ch - '0');
+		if (after_num > 0 && before_num < 0)
 			return (-1);
-		return (1);
 	}
 	return (0);
 }
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	minus;
-	int	num;
+	int		i;
+	int		minus;
+	long	num;
 
 	i = 0;
 	minus = 1;
+	num = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		++i;
 	if (str[i] == '-' || str[i] == '+')
@@ -71,8 +79,23 @@ int	ft_atoi(const char *str)
 // 	printf("%d %d %d %d %d %d %d\n", atoi(c1), atoi(c2), atoi(c3) 
 // 	, atoi(c4), atoi(c5) , atoi(c6) , atoi(c7));
 
-// 	printf ("%d ", ft_atoi("99999999999999999999"));
-// 	printf ("%d\n", ft_atoi("-99999999999999999999"));
-// 	printf ("%d ", atoi("99999999999999999999"));
-// 	printf ("%d\n", atoi("-99999999999999999999"));
+// 	printf ("%d ", ft_atoi("9223372036854775807"));			
+//// -1 (최댓값이어서 over flow 발생 안 함, 순수 -1)
+// 	printf ("%d ", ft_atoi("9223372036854775808"));			
+//// -1 (!! OVER FLOW !!)
+// 	printf ("%d ", ft_atoi("9999999999999999999999999"));	
+//// -1 (!! OVER FLOW !!)
+// 	printf ("%d ", ft_atoi("-9223372036854775808"));		
+//// 0 (최솟값이어서 under flow 발생 안 함, 순수 0)
+// 	printf ("%d ", ft_atoi("-9223372036854775809"));		
+//// 0 (!! UNDER FLOW !!)
+// 	printf ("%d\n", ft_atoi("-9999999999999999999999999"));	
+//// 0 (!! UNDER FLOW !!)
+
+// 	printf ("%d ", atoi("9223372036854775807"));
+// 	printf ("%d ", atoi("9223372036854775808"));
+// 	printf ("%d ", atoi("9999999999999999999999999"));
+// 	printf ("%d ", atoi("-9223372036854775808"));
+// 	printf ("%d ", atoi("-9223372036854775809"));
+// 	printf ("%d\n", atoi("-9999999999999999999999999"));
 // }
