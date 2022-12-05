25b7496afd5e67445df9f397c07754891c285cc1
Kwangsik Song <kwsong@c9r1s6.42seoul.kr>
2022-12-05 20:54:24 +0900

    commit

---
+++ get_next_line/main.c
@@ -0,0 +1,29 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_line(int fd)
+{
+	char *str = get_next_line(fd);
+	printf("return : %s\n-------- \n", str);
+	free(str);
+}
+
+int main()
+{
+	int fd = open("t_hello.txt", O_RDONLY);
+	printf("fd = %d\n", fd);
+
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+	print_line(fd);
+}
