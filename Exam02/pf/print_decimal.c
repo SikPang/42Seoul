c1bd34f5f043630e43503cf1cef38fa8b93005dd
SikPang <87380790+SikPang@users.noreply.github.com>
2023-01-09 22:23:13 +0900

    commit

---
+++ Exam02/pf/print_decimal.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_decimal.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 16:01:32 by kwsong            #+#    #+#             */
+/*   Updated: 2022/12/10 19:04:09 by kwsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_printf.h"
+
+static void	recursion(long long ln, int *size)
+{
+	if (ln < 10)
+	{
+		print_char(ln + '0');
+		++(*size);
+		return ;
+	}
+	recursion(ln / 10, size);
+	print_char(ln % 10 + '0');
+	++(*size);
+	return ;
+}
+
+int	print_decimal(int num, char check)
+{
+	long long	ln;
+	int			size;
+
+	size = 0;
+	if (check == 'u')
+		ln = (unsigned int)num;
+	else
+		ln = num;
+	if (ln < 0)
+	{
+		print_char('-');
+		++size;
+		ln = -1 * ln;
+	}
+	recursion(ln, &size);
+	return (size);
+}
