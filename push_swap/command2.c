74d84623f20efdb8a575ca09513add9a193dba5c
Kwangsik Song <kwsong@c3r4s3.42seoul.kr>
2023-01-09 19:24:28 +0900

    commit

--- push_swap/command2.c
+++ push_swap/command2.c
@@ -6,11 +6,28 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:44:24 by kwsong            #+#    #+#             */
-/*   Updated: 2023/01/09 18:48:10 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/01/09 19:24:21 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "command.h"
+
+static void	print_commands2(int data)
+{
+	if (data == RA)
+		write(1, "ra\n", 3);
+	else if (data == RB)
+		write(1, "rb\n", 3);
+	else if (data == RR)
+		write(1, "rr\n", 3);
+	else if (data == RRA)
+		write(1, "rra\n", 4);
+	else if (data == RRB)
+		write(1, "rrb\n", 4);
+	else if (data == RRR)
+		write(1, "rrr\n", 4);
+}
 
 void	print_commands(t_queue *que)
 {
@@ -33,20 +50,4 @@ void	print_commands(t_queue *que)
 			print_commands2(temp->data);
 		temp = temp->next_node;
 	}
-}
-
-static void	print_commands2(int data)
-{
-	if (data == RA)
-		write(1, "ra\n", 3);
-	else if (data == RB)
-		write(1, "rb\n", 3);
-	else if (data == RR)
-		write(1, "rr\n", 3);
-	else if (data == RRA)
-		write(1, "rra\n", 4);
-	else if (data == RRB)
-		write(1, "rrb\n", 4);
-	else if (data == RRR)
-		write(1, "rrr\n", 4);
 }
