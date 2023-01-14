16b811391f73e53a2356f2835771206b70b7a34d
Kwangsik Song <kwsong@c3r8s5.42seoul.kr>
2023-01-14 17:17:41 +0900

    commit

--- push_swap/sort/sort.h
+++ push_swap/sort/sort.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   sort.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 12:16:10 by kwsong            #+#    #+#             */
-/*   Updated: 2023/01/13 22:40:33 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/01/14 17:17:30 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef SORT_H
+# define SORT_H
 
 # include "../data_structure/deque.h"
 # include "../data_structure/queue.h"
