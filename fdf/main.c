94eddb48a078b88a763393928935737d77e626a7
Kwangsik Song <kwsong@c3r3s5.42seoul.kr>
2023-01-19 22:28:20 +0900

    a

--- fdf/main.c
+++ fdf/main.c
@@ -6,12 +6,20 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:27:16 by kwsong            #+#    #+#             */
-/*   Updated: 2023/01/19 17:27:39 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/01/19 20:17:57 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
+
 int main(void)
 {
-	
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 500, 500, "Hello");
+	mlx_loop(mlx);
+	mlx_destroy_window(mlx, win);
 	return (0);
 }
