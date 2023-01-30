bcd228e35acc172128d0144d8a58e8d99b0e4a09
Kwangsik Song <kwsong@c2r7s5.42seoul.kr>
2023-01-30 21:45:18 +0900

    commit

--- fdf/event.c
+++ fdf/event.c
@@ -6,7 +6,7 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:09:31 by kwsong            #+#    #+#             */
-/*   Updated: 2023/01/30 18:59:29 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/01/30 21:45:03 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 int	press_key(int key_code)
 {
 	if (key_code == KEY_ESC)
+		exit(1);
+	else if (key_code == KEY_C)	// KEY_CHANGE
+		exit(1);
+	else if (key_code == KEY_Z)	// KEY_ZOOM
+		exit(1);
+	else if (key_code == KEY_T)	// KEY_TRANSLATE
+		exit(1);
+	else if (key_code == KEY_R)	// KEY_ROTATE
+		exit(1);
+	else if (key_code == KEY_I)	// KEY_INVERT
 		exit(1);
 	return (0);
 }
--- fdf/info.h
+++ fdf/info.h
@@ -6,7 +6,7 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 18:56:24 by kwsong            #+#    #+#             */
-/*   Updated: 2023/01/30 20:46:01 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/01/30 21:45:11 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 # define X_EVENT_PRESS 2
 # define X_EVENT_EXIT 17
 # define KEY_ESC 53
+# define KEY_C 8
+# define KEY_Z 6
+# define KEY_T 17
+# define KEY_R 15
+# define KEY_I 34
 
 # define WIN_WIDTH 1000
 # define WIN_HEIGHT 1000
