a2eae9e728ea6bd9fe1bd044d6b87f561bc586c3
송광식 <87380790+SikPang@users.noreply.github.com>
2023-04-26 19:12:36 +0900

    commit

--- 4_CPP Module/CPP03/ex02/main.cpp
+++ 4_CPP Module/CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 21:01:22 by kwsong            #+#    #+#             */
-/*   Updated: 2023/02/16 17:21:24 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/04/26 19:09:46 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,65 @@ void PrevFunc()
 	std::cout << '\n';
 }
 
+void CheckBIsAlive(ScavTrap& stB)
+{
+	std::cout << "is ScavTrap" << CYN << " B " << NC << "Alive? : " << stB.IsAlive() << "\n\n";
+}
+
+void PrevFunc2()
+{
+	ScavTrap	stA("A");
+	ScavTrap	stB("B");
+
+	std::cout << '\n';
+	for (int i = 0; i < 6; ++i)
+	{
+		stA.attack("B");
+		stB.takeDamage(stA.getAttackDamage());
+		CheckBIsAlive(stB);
+	}
+
+	stB.beRepaired(20);
+	stB.guardGate();
+	stB.attack("A");
+
+	std::cout << '\n';
+	stA.beRepaired(20);
+
+	std::cout << '\n';
+	stA.guardGate();
+	stA.guardGate();
+	
+	std::cout << '\n';
+}
+
 int main()
 {
 	PrevFunc();
+	std::cout << "\n-----------------------------------\n\n";
+	PrevFunc2();
 	std::cout << "\n-----------------------------------\n\n";
 
 	FragTrap ftA("A");
 	FragTrap ftB("B");
 
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 3; ++i)
 	{
 		std::cout << '\n';
 		ftA.attack("B");
 		ftB.takeDamage(ftA.getAttackDamage());
 	}
+	ftB.beRepaired(1);
 	ftB.highFivesGuys();
+
+	std::cout << '\n';
+	ftA.attack("B");
+	ftB.takeDamage(ftA.getAttackDamage());
 	
 	std::cout << '\n';
 	ftB.beRepaired(1);
 	ftB.highFivesGuys();
+	ftB.attack("A");
 
 	std::cout << '\n';
 }
