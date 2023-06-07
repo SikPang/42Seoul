4928fba2ddd73793a549c6950ad21226883f3130
송광식 <87380790+SikPang@users.noreply.github.com>
2023-06-07 21:36:05 +0900

    commit

--- 4_CPP Module/CPP09/ex02/PmergeMe.cpp
+++ 4_CPP Module/CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 18:18:53 by kwsong            #+#    #+#             */
-/*   Updated: 2023/06/06 20:45:52 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/06/07 21:34:13 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,9 +69,26 @@ void PmergeMe::setArgs(int ac, char** str)
 	}
 }
 
+/*
+def main():
+  S = [123, 56, 45, 346, 14, 12, 312, 5, 57, 7, 9, 7856]
+  merge_insertion_sort(S)
+
+
+if __name__ == "__main__":
+  main()
+*/
+
 void PmergeMe::sort()
 {
-	
+	/*
+	1. 홀수면 straggler 정하기
+	2. 2개씩 짝짓기
+	3. 짝끼리 정렬
+	4. 각 쌍의 큰 수 비교 정렬
+	5. 각 쌍에서 큰 수는 S, 작은 수는 pend로 옮기기
+	6. 
+	*/
 }
 
 
