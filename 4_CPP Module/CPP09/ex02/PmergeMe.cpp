9a3861e009c0be49f1d18bfdadbcc83b2fc262ea
송광식 <87380790+SikPang@users.noreply.github.com>
2023-06-08 22:02:55 +0900

    commit

--- 4_CPP Module/CPP09/ex02/PmergeMe.cpp
+++ 4_CPP Module/CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: kwsong <kwsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 18:18:53 by kwsong            #+#    #+#             */
-/*   Updated: 2023/06/08 21:29:38 by kwsong           ###   ########.fr       */
+/*   Updated: 2023/06/08 22:01:23 by kwsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,16 +74,18 @@ static int jacobsthal(int n)
 	return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
 }
 
-static void getJacobSequence(int len, std::queue<int> que)
+static void getJacobSequence(int len, std::queue<int>& que)
 {
 	int jacobIndex = 3;
 
 	while (true)
 	{
-		int jacobNum = jacobsthal(jacobIndex);
-
+		int jacobNum = jacobsthal(jacobIndex) - 1;
 		if (jacobNum >= len - 1)
+		{
+			que.push(jacobNum);
 			break;
+		}
 
 		que.push(jacobNum);
 		++jacobIndex;
@@ -171,33 +173,6 @@ void PmergeMe::pushArgs(T& container)
 		container.push_back(argQue[i]);
 }
 
-/*
-1. 홀수면 straggler 정하기
-2. 2개씩 짝짓기
-3. 짝끼리 정렬
-4. 각 쌍의 큰 수 비교 정렬
-5. 각 쌍에서 큰 수는 S, 작은 수는 pend로 옮기기
-
-6. pend의 첫 요소를 S에 넣기 (첫 쌍의 작은 수는 항상 첫 쌍의 큰 수보다 작기 때문)
-7. pend의 길이에 따라서 Jacobsthal Number 수열 구하기 ( Jn = Jn-1 + 2 * Jn-2) 
-8. index[]에 1 넣어둠
-9. pend를 반복
-	만약 Jacob[]이 남아있으면서 && 플래그가 아니면
-		index[]에 Jacob 수열의 첫번째를 push_back
-		pend[Jacob 수열의 첫번째 - 1] 값을 가져옴
-		Jacob[]의 첫번째를 pop
-		플래그 세움
-	아니면
-		index[]을 순회하며 반복자와 같은 요소가 있다면 반복자 +=1
-		pend[반복자 - 1] 값을 가져옴
-		index[]에 반복자를 push_back
-		플래그 초기화
-	S에서 가져온 값이 어디에 들어가야 하는지 이진탐색 후 insert
-	반복자 += 1
-	JacobIndex +=1
-10. 만약 straggler가 있다면 S에 이진탐색 후 insert
-*/
-
 void PmergeMe::sortVector()
 {
 	clock_t begin = clock();
@@ -211,22 +186,41 @@ void PmergeMe::sortVector()
 
 	separate(vec, result, less);
 	result.insert(result.begin(), less[0]);
-	less.erase(less.begin());
+	print(result);
+	print(less);
 
 	std::queue<int> jacob;
 	getJacobSequence(less.size(), jacob);
-
 	
+	std::set<int> usedIndex;
+	usedIndex.insert(0);
 	
-	for (int i = 0; i < (int)less.size(); ++i)
+	int cur = 1;
+	while (true)
 	{
-		// Use Jacobsthal
-		std::vector<long>::iterator pos = std::upper_bound(result.begin(), result.end(), less[i]);
+		if (usedIndex.find(cur - 1) != usedIndex.end())
+		{
+			if (jacob.empty())
+				break;
+			else
+			{
+				cur = jacob.front();
+				jacob.pop();
+			}
+		}
+		else
+			cur -= 1;
+
+		while (cur >= (int)less.size())
+			--cur;
+			
+		usedIndex.insert(cur);
+		std::vector<long>::iterator pos = std::upper_bound(result.begin(), result.end(), less[cur]);
 
 		if (pos == result.end())
-			result.push_back(less[i]);
+			result.push_back(less[cur]);
 		else
-			result.insert(pos, less[i]);
+			result.insert(pos, less[cur]);
 	}
 
 	if (straggler.isExist)
@@ -238,7 +232,7 @@ void PmergeMe::sortVector()
 		else
 			result.insert(pos, straggler.num);
 	}
-
+	print(result);
 	clock_t end = clock();
 	unsigned long elapsedMicroseconds = ticksToMicroseconds(end - begin);
     std::cout <<  "Time to process a range of "<< result.size() << " elements with std::vector : "  << elapsedMicroseconds << " μs\n";
