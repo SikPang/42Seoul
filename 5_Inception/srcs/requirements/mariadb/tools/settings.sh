e365945b99dd0bee02c8342b9df1062b6e04a688
Kwangsik Song <kwsong@c3r6s2.42seoul.kr>
2023-06-27 13:44:57 +0900

    commit

--- 5_Inception/srcs/requirements/mariadb/tools/settings.sh
+++ 5_Inception/srcs/requirements/mariadb/tools/settings.sh
@@ -1,16 +1,18 @@
 #!/bin/bash 
 
-mariadb-install-db
+if [ ! -d /var/lib/mysql/${WORDPRESS_DB_NAME} ]; then
+    mariadb-install-db
 
-service mariadb start
+    service mariadb start
 
-mysql -u root -e "set password for 'root'@'localhost' = password('$MYSQL_ROOT_PASSWORD');"
-mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
-mysql -u root -e "CREATE user $WORDPRESS_DB_USER@'%' identified by '$WORDPRESS_DB_PASSWORD';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "set password for 'root'@'localhost' = password('$MYSQL_ROOT_PASSWORD');"
+    mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
+    mysql -u root -e "CREATE user $WORDPRESS_DB_USER@'%' identified by '$WORDPRESS_DB_PASSWORD';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
-service mariadb stop
+    service mariadb stop
+fi
 
 mysqld --user=root
 
