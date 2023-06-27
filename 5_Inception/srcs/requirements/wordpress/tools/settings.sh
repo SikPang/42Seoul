5eec6d57f052ee0156dcfe02e000c4f69f0dc601
Kwangsik Song <kwsong@c3r6s2.42seoul.kr>
2023-06-27 22:55:57 +0900

    commit

--- 5_Inception/srcs/requirements/wordpress/tools/settings.sh
+++ 5_Inception/srcs/requirements/wordpress/tools/settings.sh
@@ -1,13 +1,15 @@
 #!/bin/bash 
 
-if [ ! -c /var/www/html/wordpress/index.php ]; then
+if [ ! -e /var/www/html/wordpress/index.php ]; then
     wp core download --allow-root --path=/var/www/html/wordpress/
-    
-    mv /var/www/html/wp-config.php /var/www/html/wordpress/wp-config.php 
+    mv /var/www/html/wordpress/wp-config-sample.php  /var/www/html/wordpress/wp-config.php 
+    #mv /var/www/html/wp-config.php /var/www/html/wordpress/wp-config.php 
     
     chown www-data: /var/www/html/ -R
-
+    
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --locale=en_US --allow-root --path=/var/www/html/wordpress/
     wp core install --url=localhost --title=my_blog --admin_email=kwsong@42.fr --admin_password=1234 --admin_name=song --allow-root --path=/var/www/html/wordpress/
+    wp user create human human@42.fr --user_pass=1234 --role=subscriber --allow-root --path=/var/www/html/wordpress/
 fi
 
 /usr/sbin/php-fpm7.4 -F
